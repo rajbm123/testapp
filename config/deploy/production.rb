4fb41f89ece15f27b201f1f90ecf8d6bce97058d
rajbm123 <rajbm8147898797@gmail.com>
2016-09-26 01:25:51 +0530

    deploy one

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,8 @@
+set :stage, :production
+
+# Replace 127.0.0.1 with your server's IP address!
+server '103.14.196.158', user: 'gcn', roles: %w{web app db}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
